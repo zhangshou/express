edb222fcf8a51142ac9f0bc6d232581d62198375
zhangshou <83363694@qq.com>
2016-11-12 12:38:38 +0800

    中间件

--- 1.express/1.express.js
+++ 1.express/1.express.js
@@ -1,6 +1,26 @@
 var express=require('express');
 
 var app=express();
+
+//使用中间件
+//next是一个函数,如果调用它表示中间件执行完毕,可以继续向下
+//路由把公用的代码写在中间件里面
+/*
+* 中间件的功能
+* 1.增加公共的方法和属性
+* 2.进行公共的处理
+* */
+/*
+* 路由和中间件的区别和联系
+* 1.他们在同一个数组中
+* 2.中间件不匹配路径和方法名,路由匹配路径和方法名
+* 3.中间件多了next参数,它能决定是否继续
+* */
+app.use(function (req,res,next) {
+    res.setHeader('Content-Type','text/plain;charset=utf8');
+    next();
+});
+
 ///当客户端通过get的请求方式,访问路径的时候
 app.get('/', function (req, res) {
     res.end('首页')
@@ -8,6 +28,8 @@ app.get('/', function (req, res) {
 app.get('/login', function (req,res) {
     res.end('登录1')
 });
+
+//curl -X POST http://localhost:8090/login 测试post请求
 app.post('/login', function (req,res) {
     res.end('登录2')
 });
